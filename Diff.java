efbc20a91478302d68903664689afb841db4ac4c
Pratap Amare <pratap.amare@gmail.com>
2021-08-22 14:38:29 +0530

    adding diff java

---
+++ Diff.java
@@ -0,0 +1,10 @@
+public class Diff {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, dif;
+      dif =  num2-num1;
+
+      System.out.println("Sum numbers: "+dif);
+   }
+}
