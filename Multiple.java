5c206f3c6ffcefacbc4470d6c06f51eab4f6af96
Pratap Amare <pratap.amare@gmail.com>
2021-08-22 16:50:47 +0530

    make changes in Multiple

--- Multiple.java
+++ Multiple.java
@@ -5,6 +5,6 @@ public class Multiple {
       int num1 = 5, num2 = 15, mult;
       mult = num1 * num2;
 
-      System.out.println("mult numbers: "+mult);
+      System.out.println("mult numbers making some changes: "+mult);
    }
 }
